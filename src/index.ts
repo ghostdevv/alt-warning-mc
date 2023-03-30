ac68c4a079b64124161c2a3c3ede7a55fbde7d4e
GHOST <ghostdevbusiness@gmail.com>
2023-03-30 15:21:02 +0000

    feat: worker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-export default {}
+import { MCStatusResponse } from './types';
+
+const ALTS = ['Appetising', 'sectum_sempra'];
+const SERVER = '157.90.3.16:25661';
+
+async function sendWebhook(alt: string) {
+    await fetch('https://webhook.willow.sh', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            Status: 'Alt Missing',
+            Server: SERVER,
+            Alt: alt,
+        }),
+    });
+}
+
+async function check() {
+    const response = await fetch(
+        `https://api.mcstatus.io/v2/status/java/${SERVER}`,
+    );
+
+    const data = await response.json<MCStatusResponse>();
+
+    const players = data.players.list.map((player) => player.name_raw);
+
+    for (const alt of ALTS) {
+        if (!players.includes(alt)) {
+            await sendWebhook(alt);
+        }
+    }
+}
+
+export default {
+    scheduled: check,
+};
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,39 @@
-export {}
+export interface MCStatusResponse {
+    online: boolean;
+    host: string;
+    port: number;
+    eula_blocked: boolean;
+    retrieved_at: number;
+    expires_at: number;
+    version: Version;
+    players: Players;
+    motd: Motd;
+    icon: string;
+    mods: any[];
+}
+
+export interface Version {
+    name_raw: string;
+    name_clean: string;
+    name_html: string;
+    protocol: number;
+}
+
+export interface Players {
+    online: number;
+    max: number;
+    list: List[];
+}
+
+export interface List {
+    uuid: string;
+    name_raw: string;
+    name_clean: string;
+    name_html: string;
+}
+
+export interface Motd {
+    raw: string;
+    clean: string;
+    html: string;
+}
